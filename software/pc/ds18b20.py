37852e3ddaddbddb1728919eee07267eab623ae0
Brollof <thefish3r@gmail.com>
2018-03-26 21:19:37 +0200

    Reading temperature from sensors

---
+++ software/pc/ds18b20.py
@@ -0,0 +1,58 @@
+import os
+import glob
+import time
+import logging
+
+class DS18B20():
+    def __init__(self):
+        self.log = logging.getLogger('DS18B20')
+        if os.name == "unix": # rpi
+            baseDir = '/sys/bus/w1/devices/'
+        else:
+            baseDir = './sys/bus/w1/devices/' # sensors simulation
+
+        deviceFolders = glob.glob(baseDir + '28*')
+        deviceFiles = [folder + '/w1_slave' for folder in deviceFolders]
+        names = [folder[-15:] for folder in deviceFolders]
+        self.devices = dict(zip(names, deviceFiles))
+
+    def __readTempsRaw(self):
+        ret = {}
+        for name, dev in self.devices.items():
+            try:
+                f = open(dev, 'r')
+                ret[name] = f.readlines()
+                f.close()
+            except Exception as e:
+                self.log.error(e)
+        return ret
+
+    def readTemps(self):
+        temps = {}
+        rawData = self.__readTempsRaw()
+        for name, temp in rawData.items():
+            while rawData[name][0].strip()[-3:] != 'YES':
+                time.sleep(0.2)
+                rawData = self.__readTempsRaw()
+
+            equalsPos = rawData[name][1].find('t=')
+            if equalsPos != -1:
+                tempStr = rawData[name][1][equalsPos + 2:]
+                temps[name] = float(tempStr) / 1000.0
+        return temps
+
+def printPeriodic(ds):
+    for i in range(3):
+        temps = ds.readTemps()
+        for n, t in temps.items():
+            print('name: %s, temp: %3.1f*C' % (n, t))
+        print('------------------------------------------')
+        time.sleep(1)
+
+def main():
+    ds = DS18B20()
+    print(ds.readTemps())
+    printPeriodic(ds)
+
+if __name__ == '__main__':
+    main()
---
+++ software/pc/ter_temp.py
@@ -0,0 +1,34 @@
+from ter_io import AsyncIO
+import logging
+import ds18b20
+
+class TempSensorsManager():
+    QUEUE_CHECK_PERIOD = 0.2 # seconds
+
+    def __init__(self):
+        self.log = logging.getLogger('Temp Thread')
+        self.ds = ds18b20.DS18B20()
+        self.asyncIO = AsyncIO(self.QUEUE_CHECK_PERIOD, self.__onRead, None, self.__onStart)
+
+    def __onRead(self, data):
+        return self.ds.readTemps()
+
+    def __onStart(self):
+        self.log.info('Temp sensor thread started')
+        print('Started')
+
+    def read(self):
+        return self.asyncIO.read(None)
+
+    def close(self):
+        self.asyncIO.close()
+
+def main():
+    from time import sleep
+    tman = TempSensorsManager()
+    print(tman.read())
+    sleep(1)
+    tman.close()
+
+if __name__ == '__main__':
+    main()
