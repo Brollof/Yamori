29396625f3d5f5b35a07dce47829f1e4f59bcf75
Brollof <thefish3r@gmail.com>
2018-11-10 22:05:40 +0100

    Tools added: cpu temp logger

---
+++ software/tools/cpu_temp.py
@@ -0,0 +1,36 @@
+import sys
+import subprocess
+import time
+from datetime import datetime
+
+LOG_FILE = 'cpu_temp.log'
+TIME_LOG_PERIOD = 60 # seconds
+verbose = False
+
+def get_temp():
+    a = subprocess.run('vcgencmd measure_temp', stdout=subprocess.PIPE, shell=True, check=True, universal_newlines=True)
+    return a.stdout.strip()
+
+def get_time():
+    return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def write_data():
+    with open(LOG_FILE, 'a+') as f:
+        data = '{}: {}\n'.format(get_time(), get_temp())
+        if verbose:
+            print('Writing: %s' % data, end='')
+        f.write(data)
+
+if __name__ == '__main__':
+    try:
+        verbose = sys.argv[1]
+    except Exception:
+        pass
+
+    if verbose:
+        print('CPU temperature logger. Logging to file: %s' %LOG_FILE)
+        print('Running in verbose mode')
+
+    while True:
+        write_data()
+        time.sleep(TIME_LOG_PERIOD)
