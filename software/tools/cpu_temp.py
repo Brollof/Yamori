92d0bf77b46bec5870570bf45198a70de3acf752
Brollof <thefish3r@gmail.com>
2018-11-11 11:49:04 +0100

    cpu temp tool: fixed logging to file

--- software/tools/cpu_temp.py
+++ software/tools/cpu_temp.py
@@ -1,10 +1,14 @@
 import sys
+import os
 import subprocess
 import time
 from datetime import datetime
 
 LOG_FILE = 'cpu_temp.log'
 TIME_LOG_PERIOD = 60 # seconds
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+FULL_LOG_PATH = os.path.join(DIR_PATH, LOG_FILE)
+
 verbose = False
 
 def get_temp():
@@ -15,7 +19,7 @@ def get_time():
     return datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def write_data():
-    with open(LOG_FILE, 'a+') as f:
+    with open(FULL_LOG_PATH, 'a+') as f:
         data = '{}: {}\n'.format(get_time(), get_temp())
         if verbose:
             print('Writing: %s' % data, end='')
@@ -28,7 +32,7 @@ if __name__ == '__main__':
         pass
 
     if verbose:
-        print('CPU temperature logger. Logging to file: %s' %LOG_FILE)
+        print('CPU temperature logger. Logging to file: %s' %FULL_LOG_PATH)
         print('Running in verbose mode')
 
     while True:
